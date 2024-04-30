e6635aad9c9ebe91ef5f21d8ce5a820d073f316f
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-04-30 23:13:33 +0100

    restful

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,36 @@
+
 #!/usr/bin/python3
+"""Flask server (variable app)
 """
-create Flask app; and register the blueprint app_views to flask instance app.^
-"""
-from os import  getenv
-from flask import Flask
+
+
+from flask import Flask, jsonify
 from models import storage
-from api.vi.views import app_views
+from os import getenv
+from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def downtear(self):
+    '''Status of your API'''
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify(error='Not found'), 404
 
 
 if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT' '5000'))
-    app.run(host=HOST, port=PORT, threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python3
-"""
-create Flask app; and register the blueprint app_views to flask instance app.^
-"""
+# #!/usr/bin/python3
+# """
+# create Flask app; and register the blueprint app_views to flask instance app.^
+# """
+# from flask import Blueprint
+
+# app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# from api.v1.views.index import *
+
 from flask import Blueprint
 
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 
 from api.v1.views.index import *
